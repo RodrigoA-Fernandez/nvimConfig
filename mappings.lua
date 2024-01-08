8b6fa029056df40b6f5fbc34bae59e9c8e4e2975
Rodrigo <rodrigoalejandro.fernandez@estudiantes.uva.es>
2024-01-09 00:49:17 +0100

    Añadido mapping para expandir diagnóstico

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["md"] = { "<Cmd>lua vim.diagnostic.open_float() <CR>", "Expandir Diagnóstico", opts = {nowait = true }},
   },
 }
 
