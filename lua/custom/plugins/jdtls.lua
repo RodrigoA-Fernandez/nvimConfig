04edcfbd414c0b5363687df21ae2a80d2186a7c8
Rodrigo F <rodrigoalejandro.fernandez@estudiantes.uva.es>
2024-04-14 22:06:47 +0200

    JavaWindows

--- lua/custom/plugins/jdtls.lua
+++ lua/custom/plugins/jdtls.lua
@@ -5,18 +5,41 @@ return{
     'mfussenegger/nvim-dap',
   },
   config = function ()
-    local home = os.getenv 'HOME'
-    local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
     local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-    local workspace_dir = workspace_path .. project_name
 
     local status, jdtls = pcall(require, 'jdtls')
     if not status then
       return
     end
     local extendedClientCapabilities = jdtls.extendedClientCapabilities
+    local cmd; 
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+      cmd = {
+        '',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xmx1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens',
+        'java.base/java.util=ALL-UNNAMED',
+        '--add-opens',
+        'java.base/java.lang=ALL-UNNAMED',
+        '-javaagent:' .. vim.fn.glob(vim.fn.expand(vim.fn.stdpath('data').."/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")),
+        '-jar',
+        vim.fn.glob(vim.fn.expand(vim.fn.stdpath('data').."/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")),
+        '-configuration',
+        vim.fn.glob(vim.fn.expand(vim.fn.stdpath("data")..'/mason/packages/jdtls/config_linux')),
+        '-data',
+        vim.fn.glob(vim.fn.expand(vim.fn.stdpath('data').."/jdtls-workspace/"..project_name)),
+      }
+    else
+      local home = os.getenv 'HOME'
+      local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+      local workspace_dir = workspace_path .. project_name
 
-    local config = {
       cmd = {
         'java',
         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
@@ -37,7 +60,11 @@ return{
         home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
         '-data',
         workspace_dir,
-      },
+      }
+    end
+
+    local config = {
+      cmd = cmd,
       root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
 
       settings = {
