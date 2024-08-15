4f8fb8b79bce5228cec606464a2641a5a7f9f039
Rodrigo Fernández <rodrigoalejandro.fernandez@estudiantes.uva.es>
2024-08-15 20:03:36 +0200

    Ultima actualizacion fedora wsl

--- lua/custom/plugins/load_test_lsp.lua
+++ lua/custom/plugins/load_test_lsp.lua
@@ -1,17 +1,17 @@
--- local client = vim.lsp.start_client {
---   name = 'natural_language_lsp',
---   cmd = { '/home/rodrigo/Proyectos/natural_language_lsp/main' },
--- }
---
--- if not client then
---   vim.notify 'Que coño has hecho'
---   return
--- end
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = 'markdown',
---   callback = function()
---     vim.lsp.buf_attach_client(0, client)
---   end,
--- })
+local client = vim.lsp.start_client {
+  name = 'natural_language_lsp',
+  cmd = { '/home/rodrigo/Proyectos/natural_language_lsp/natural_language_lsp' },
+}
+
+if not client then
+  vim.notify 'Que coño has hecho'
+  return
+end
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.lsp.buf_attach_client(0, client)
+  end,
+})
 
 return {}
